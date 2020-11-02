c6a6227a89e5f534ba8506c80afcede6be9860c7
HondaNonoho <nnhhnd3012@gmail.com>
2020-11-02 18:38:08 +0900

    DOTween

---
+++ DOTween20201012/Assets/Script/DOTweenController.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class SlideController : MonoBehaviour
+{
+    [SerializeField] private RectTransform _SlidebarImage = null; //RectTransformとしてImageを宣言
+    public void HomeSlide()
+    {
+        //RectTransform rt = _SridebarImage.GetComponent<RectTransform>(); →RectTransformをゲット済
+        Vector3 start = _SlidebarImage.localPosition;
+        _SlidebarImage.localPosition = start;
+
+            if(start.x == -187) //watch
+            {
+               start.x -= 125;
+            }else if(start.x == -61){ //MyPage
+                start.x -= 251;
+            }else if(start.x == 63){ //Group
+                start.x -= 375;
+            }else if(start.x == 188){ //Information
+                start.x -= 500;
+            }else if(start.x == 313){ //Menu
+                start.x -= 625;
+            }
+        Vector3 end = _SlidebarImage.localPosition;
+        _SlidebarImage.DOLocalMove(start, 0.5f);   
+    }
+
+    public void WatchSlide()
+    {
+        
+            Vector3 start = _SlidebarImage.localPosition;
+            _SlidebarImage.localPosition = start;
+            
+            if(start.x == -312)//HomeButton
+            {
+                start.x += 125;
+            }else if(start.x == -61){ //MyPage
+                start.x -= 126;
+            }else if(start.x == 63){ //Group
+                start.x -= 250;
+            }else if(start.x == 188){ //Information
+                start.x -= 375;
+            }else if(start.x == 313){ //Menu
+                start.x -= 500;
+            }
+            
+            
+            Vector3 end = _SlidebarImage.localPosition;
+            _SlidebarImage.DOLocalMove(start, 0.5f);        
+    }
+
+    public void MypageSlide()
+    {
+            Vector3 start = _SlidebarImage.localPosition;
+            _SlidebarImage.localPosition = start;
+            
+            if(start.x == -312)//HomeButton
+            {
+                start.x += 251;
+            }else if(start.x == -187){ //Watch
+                start.x += 126;
+            }else if(start.x == 63){ //Group
+                start.x -= 124;
+            }else if(start.x == 188){ //Information
+                start.x -= 249;
+            }else if(start.x == 313){ //Menu
+                start.x -= 374;
+            }
+           
+             Vector3 end = _SlidebarImage.localPosition;
+            _SlidebarImage.DOLocalMove(start, 0.5f);
+    }
+
+    public void GroupSlide()
+    {
+            Vector3 start = _SlidebarImage.localPosition;
+            _SlidebarImage.localPosition = start;
+            //start.x += 375;
+            if(start.x == -312)//HomeButton
+            {
+                start.x += 375;
+            }else if(start.x == -187){ //Watch
+                start.x += 250;
+            }else if(start.x == -61){ //MyPage
+                start.x += 124;
+            }else if(start.x == 188){ //Information
+                start.x -= 125;
+            }else if(start.x == 313){ //Menu
+                start.x -= 250;
+            }
+            
+            Vector3 end = _SlidebarImage.localPosition;
+            _SlidebarImage.DOLocalMove(start, 0.5f); 
+        
+    }
+
+    public void InformationSlide()
+    {
+            Vector3 start = _SlidebarImage.localPosition;
+            _SlidebarImage.localPosition = start;
+            
+            if(start.x == -312)//HomeButton
+            {
+                start.x += 500;
+            }else if(start.x == -187){ //Watch
+                start.x += 375;
+            }else if(start.x == -61){ //MyPage
+                start.x += 249;
+            }else if(start.x == 63){ //Group
+                start.x += 125;
+            }else if(start.x == 313){ //Menu
+                start.x -= 125;
+            }
+           
+            Vector3 end = _SlidebarImage.localPosition;
+            _SlidebarImage.DOLocalMove(start, 0.5f);
+    }
+
+    public void MenuSlide()
+    {
+            Vector3 start = _SlidebarImage.localPosition;
+            _SlidebarImage.localPosition = start;
+            
+             if(start.x == -312)//HomeButton
+            {
+                start.x += 625;
+            }else if(start.x == -187){ //Watch
+                start.x += 500;
+            }else if(start.x == -61){ //MyPage
+                start.x += 374;
+            }else if(start.x == 63){ //Group
+                start.x += 250;
+            }else if(start.x == 188){ //Information
+                start.x += 125;
+            }
+            
+            Vector3 end = _SlidebarImage.localPosition;
+            _SlidebarImage.DOLocalMove(start, 0.5f);
+    }
+}
---
+++ DOTween20201012/Assets/Script/TestController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TestController : MonoBehaviour
+{
+    [SerializeField] private Button _Button1 = null;
+    [SerializeField] private Button _Button2 = null;
+    [SerializeField] private Button _Button3 = null;
+    [SerializeField] private Button _Button4 = null;
+    [SerializeField] private Button _Button5 = null; 
+    
+
+
+
+
+    
+    void Start()
+    {
+        _Button1.onClick.AddListener(onClick1);
+        _Button2.onClick.AddListener(onClick2);
+         _Button3.onClick.AddListener(onClick3);
+          _Button4.onClick.AddListener(onClick4);
+           _Button5.onClick.AddListener(onClick5);
+    
+
+       transform.DOLocalMove(new Vector3(0, -590, 0), 0.5f);
+
+        
+    }
+
+    void onClick1()
+    {
+    transform.DOLocalMove(new Vector3(-298, -590, 0), 0.5f);
+
+    }
+
+    void onClick2()
+    {
+        transform.DOLocalMove(new Vector3(-148, -590, 0), 0.5f);
+    }
+    void onClick3()
+    {
+       transform.DOLocalMove(new Vector3(0, -590, 0), 0.5f);
+    }
+    void onClick4()
+    {
+        transform.DOLocalMove(new Vector3(148, -590, 0), 0.5f);
+    }
+    void onClick5()
+    {
+        transform.DOLocalMove(new Vector3(298, -590, 0), 0.5f);
+    }
+
+    
+}
